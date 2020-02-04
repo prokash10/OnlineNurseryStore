6a71362a4381b720ce3c975cf847695ffbe835db
prokash10 <prokasnjr10@gmail.com>
2020-02-04 10:48:14 +0545

    Commit

--- app/src/main/java/com/example/onlinenurserystore/adapter/SliderAdapter.java
+++ app/src/main/java/com/example/onlinenurserystore/adapter/SliderAdapter.java
@@ -1,4 +1,53 @@
 package com.example.onlinenurserystore.adapter;
 
-public class SliderAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.onlinenurserystore.R;
+
+public class SliderAdapter extends PagerAdapter {
+    private Context context;
+    private LayoutInflater layoutInflater;
+    private Integer [] image={R.drawable.sliderr,R.drawable.slider,R.drawable.sliders};
+
+
+    public SliderAdapter(Context context){this.context=context;}
+
+
+    @Override
+    public int getCount() {
+        return image.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view==object;
+    }
+    @Override
+    public Object instantiateItem(ViewGroup container, final int position){
+        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.custom_layout, null);
+        ImageView imageView =  view.findViewById(R.id.imageView);
+        imageView.setImageResource(image[position]);
+
+        ViewPager vp = (ViewPager) container;
+        vp.addView(view, 0);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+
+        ViewPager vp = (ViewPager) container;
+        View view = (View) object;
+        vp.removeView(view);
+
+    }
 }
