6e6c148ef2033ecbac0210f3b221f22949d7e08e
prokash10 <prokasnjr10@gmail.com>
2020-02-04 15:51:23 +0545

    Commit

--- app/src/main/java/com/example/onlinenurserystore/MainActivity.java
+++ app/src/main/java/com/example/onlinenurserystore/MainActivity.java
@@ -1,30 +1,94 @@
 package com.example.onlinenurserystore;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.example.onlinenurserystore.ui.account.AccountFragment;
+import com.example.onlinenurserystore.ui.cart.CartFragment;
+import com.example.onlinenurserystore.ui.home.HomeFragment;
+import com.example.onlinenurserystore.ui.order.OrderFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+        bottomNavigationView =findViewById(R.id.nav_view);
+        Bundle bundle=getIntent().getExtras();
+
+
+        if(bundle!=null) {
+            String name = bundle.getString("name");
+            if (name.equals("home")) {
+                bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+                openFragment(new HomeFragment());
+
+
+            } else if (name.equals("account")) {
+                bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+                bottomNavigationView.setSelectedItemId(R.id.navigation_account);
+                openFragment(new AccountFragment());
+
+            } else if (name.equals("cart")){
+                bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+                bottomNavigationView.setSelectedItemId(R.id.navigation_cart);
+                openFragment(new CartFragment());
+
+            }
+
+        }else{
+
+            bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+            openFragment(new HomeFragment());
+
+        }
+
+
+
+
+
+
     }
+
+    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.navigation_home:
+                            openFragment(new HomeFragment());
+                            return true;
+                        case R.id.navigation_message:
+                            openFragment(new OrderFragment());
+                            return true;
+                        case R.id.navigation_cart:
+                            openFragment(new CartFragment());
+                            return true;
+                        case R.id.navigation_account:
+                            openFragment(new AccountFragment());
+                            return true;
+                    }
+                    return false;
+                }
+            };
+
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.commit();
+    }
+
+
 
 }
