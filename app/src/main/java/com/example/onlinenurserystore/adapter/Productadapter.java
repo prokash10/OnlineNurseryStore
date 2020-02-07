249fe07a7f634eb00f591e63e9e191ef91d23cf9
prokash10 <prokasnjr10@gmail.com>
2020-02-07 21:15:59 +0545

    product adapter

--- app/src/main/java/com/example/onlinenurserystore/adapter/Productadapter.java
+++ app/src/main/java/com/example/onlinenurserystore/adapter/Productadapter.java
@@ -17,49 +17,49 @@ import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
-public class Productadapter extends RecyclerView.Adapter<Productadapter.ItemViewHolder>  {
+public class Productadapter extends RecyclerView.Adapter<Productadapter.ItemViewHolder> {
 
     List<Products> productsList;
     Context context;
 
-    public Productadapter(Context context, List<Products> productsLis) {
-        this.productsList = productsLis;
+    public Productadapter(Context context, List<Products> productsList) {
+        this.productsList = productsList;
         this.context = context;
     }
 
 
-
     @NonNull
     @Override
-    public Productadapter.ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.product_layout,parent, false);
+    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int position) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_plant, null);
         return new ItemViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull Productadapter.ItemViewHolder holder, int position) {
-        final Products products=productsList.get(position);
-        Picasso.get().load(Url.base_url_image+productsList.get(position).getProductImage()).into(holder.imgImage);
-        holder.tvDescription.setText(products.getProductDescription());
+    public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
+        final Products products = productsList.get(position);
+        holder.tvPlantname.setText(products.getProductName());
         holder.tvRate.setText("Rs" + products.getPrice());
+        Picasso.get().load(Url.base_url_image + productsList.get(position).getProductImage()).into(holder.imgplant);
 
     }
 
     @Override
     public int getItemCount() {
-        return productsList.size();
+        return productsList.size(); }
 
-    }
 
     public class ItemViewHolder extends RecyclerView.ViewHolder {
-        ImageView imgImage;
-        TextView tvDescription, tvRate;
+        ImageView imgplant;
+        TextView tvPlantname, tvRate;
 
         public ItemViewHolder(@NonNull View itemView) {
             super(itemView);
-            imgImage = itemView.findViewById(R.id.imgProduct);
-            tvDescription = itemView.findViewById(R.id.tvProductDesc);
-            tvRate = itemView.findViewById(R.id.tvProductPrice);
+            imgplant = itemView.findViewById(R.id.imgItem);
+            tvPlantname = itemView.findViewById(R.id.tvItemName);
+            tvRate = itemView.findViewById(R.id.tvItemPrice);
         }
     }
+
+
 }
