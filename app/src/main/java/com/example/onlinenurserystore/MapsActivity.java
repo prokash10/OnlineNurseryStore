e32ac01154ceec0fdc659ee1d39d574b97a36e91
prokash10 <prokasnjr10@gmail.com>
2020-02-22 23:25:44 +0545

    commit

--- app/src/main/java/com/example/onlinenurserystore/MapsActivity.java
+++ app/src/main/java/com/example/onlinenurserystore/MapsActivity.java
@@ -4,12 +4,17 @@ import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
+import com.example.onlinenurserystore.model.LatLong;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -39,9 +44,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng Nepal = new LatLng(-27.7003387, 85.3292906);
-        mMap.addMarker(new MarkerOptions().position(Nepal).title("Nursery Store"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(Nepal));
+        List<LatLong> latlngs = new ArrayList<>();
+        latlngs.add(new LatLong(27.706195, 85.3278509, "Softwarica College Of IT & E-Commerce"));
+        latlngs.add(new LatLong(27.717244, 85.3239595, "XYZ Nursery Store"));
+        latlngs.add(new LatLong(27.724086, 85.2939873, "Paradise Nursery\n"));
+        latlngs.add(new LatLong(27.690162, 85.3047993, "All Plants Hub"));
+        latlngs.add(new LatLong(27.6901614, 85.3047993, "Flowers Centre - New Baneshwor\n"));
+        latlngs.add(new LatLong(27.7145697, 85.3132025, "Evergreen Store\n"));
+        latlngs.add(new LatLong(27.7145697, 85.3132025, "Nursery house"));
+        latlngs.add(new LatLong(27.7151036, 85.3188539, "Take Away Plants Nepal"));
+
+        CameraUpdate center, zoom;
+        for (int i = 0; i < latlngs.size(); i++) {
+            center = CameraUpdateFactory.newLatLng(new LatLng(latlngs.get(i).getLat(), latlngs.get(i).getLon()));
+            zoom = CameraUpdateFactory.zoomTo(10);
+            mMap.addMarker(new MarkerOptions().position(new LatLng(latlngs.get(i).getLat(), latlngs.get(i).getLon()))
+                    .title(latlngs.get(i).getMarker()));
+
+            mMap.moveCamera(center);
+            mMap.animateCamera(zoom);
+            mMap.getUiSettings().setZoomControlsEnabled(true);
+        }
+
     }
 }
